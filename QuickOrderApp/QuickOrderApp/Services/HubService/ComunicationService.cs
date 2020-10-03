75e2fc6101671a232e4687df1fc8e8254141a4a1
JuanPabloTorres <est.juanpablotorres@gmail.com>
2020-10-03 15:55:42 -0400

    Hosting Service url strings.

--- QuickOrderApp/QuickOrderApp/Services/HubService/ComunicationService.cs
+++ QuickOrderApp/QuickOrderApp/Services/HubService/ComunicationService.cs
@@ -17,10 +17,10 @@ namespace QuickOrderApp.Services.HubService
 
         public ComunicationService()
         {
-          
-            hubConnection = new HubConnectionBuilder().WithUrl("http://192.168.1.144:5000" + "/comunicationhub").Build();
 
-            //hubConnection = new HubConnectionBuilder().WithUrl("http://juantorres94-001-site1.htempurl.com" + "/comunicationhub").Build();
+        //hubConnection = new HubConnectionBuilder().WithUrl("http://192.168.1.144:5000" + "/comunicationhub").Build();
+
+            hubConnection = new HubConnectionBuilder().WithUrl("http://juantorres9-001-site1.etempurl.com" + "/comunicationhub").Build();
 
             notificationManager = DependencyService.Get<INotificationManager>();
 
