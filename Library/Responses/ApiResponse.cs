85e177d9656e4902344f182de8a5a1c6c4b953a6
Juan Pablo Torres <Est.JuanPabloTorres@gmail.com>
2022-11-22 12:36:54 -0400

    Response Class Creation.

---
+++ Library/Responses/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Responses
+{
+    public class ApiResponse<T> where T :class
+    {
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+    }
+}
